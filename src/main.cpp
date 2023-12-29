8f841bcebc6d11c0d869f8ca63912969340e18b1
nagaokambeyond <40172017+nagaokambeyond@users.noreply.github.com>
2023-12-29 20:08:53 +0900

    `return`に相当するtoken作成

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,60 @@
 #include <fstream>
 #include <iostream>
+#include <optional>
 #include <sstream>
+#include <vector>
+
+enum class TokenType { _return, int_lit, semi };
+
+struct Token {
+  TokenType type;
+  std::optional<std::string> value = {};
+};
+
+std::vector<Token> tokenize(const std::string &str) {
+  std::vector<Token> tokens = {};
+  std::string buf = "";
+  for (int i = 0; i < str.length(); i++) {
+    char c = str.at(i);
+    if (std::isalpha(c)) {
+      buf.push_back(c);
+      i++;
+      while (std::isalnum(str.at(i))) {
+        buf.push_back(str.at(i));
+        i++;
+      }
+      i--;
+
+      if (buf == "return") {
+        tokens.push_back({.type = TokenType::_return});
+        buf.clear();
+        continue;
+      } else {
+        std::cerr << "You messed up!" << std::endl;
+        exit(EXIT_FAILURE);
+      }
+    } else if (std::isdigit(c)) {
+      buf.push_back(c);
+      i++;
+      while (std::isdigit(str.at(i))) {
+        buf.push_back(str.at(i));
+        i++;
+      }
+      i--;
+      tokens.push_back({.type = TokenType::int_lit, .value = buf});
+      buf.clear();
+    } else if (c == ';') {
+      tokens.push_back({.type = TokenType::semi});
+    } else if (std::isspace(c)) {
+      continue;
+    } else {
+      std::cerr << "You messed up!" << std::endl;
+      exit(EXIT_FAILURE);
+    }
+  }
+
+  return tokens;
+}
 
 int main(int argc, char *argv[]) {
   if (argc != 2) {
@@ -17,6 +71,7 @@ int main(int argc, char *argv[]) {
     contents = contents_stream.str();
   }
 
-  std::cout << contents << std::endl;
+  std::vector<Token> thing = tokenize(contents);
+
   return EXIT_SUCCESS;
 }
