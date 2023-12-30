fa96756847f9b0704a62c479c3ff0f624c525ff0
nagaokambeyond <40172017+nagaokambeyond@users.noreply.github.com>
2023-12-31 00:18:14 +0900

    tokenからassemblyを生成し、nasmでコンパイル、ldでリンク

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,27 @@ struct Token {
   std::optional<std::string> value = {};
 };
 
+std::string tokens_to_asm(const std::vector<Token> &tokens) {
+  std::stringstream output;
+  output << "global _start\n_start:\n";
+
+  for (int i = 0; i < tokens.size(); i++) {
+    const Token &token = tokens.at(i);
+    if (token.type == TokenType::_return) {
+      if (i + 1 < tokens.size() &&
+          tokens.at(i + 1).type == TokenType::int_lit) {
+        if (i + 2 < tokens.size() && tokens.at(i + 2).type == TokenType::semi) {
+          output << "    mov rax, 60\n";
+          output << "    mov rdi, " << tokens.at(i + 1).value.value() << "\n";
+          output << "    syscall";
+        }
+      }
+    }
+  }
+
+  return output.str();
+}
+
 std::vector<Token> tokenize(const std::string &str) {
   std::vector<Token> tokens = {};
   std::string buf = "";
@@ -71,7 +92,14 @@ int main(int argc, char *argv[]) {
     contents = contents_stream.str();
   }
 
-  std::vector<Token> thing = tokenize(contents);
+  std::vector<Token> tokens = tokenize(contents);
+  {
+    std::fstream file("out.asm", std::ios::out);
+    file << tokens_to_asm(tokens);
+  }
+
+  system("nasm -f elf64 out.asm");
+  system("ld -o out out.o");
 
   return EXIT_SUCCESS;
 }
