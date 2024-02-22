1cbe672d232db138d9084a7e19f51e0eae7fc410
nagaokambeyond <40172017+nagaokambeyond@users.noreply.github.com>
2024-02-23 08:12:48 +0900

    リファクタリング　tokenize

--- readme.md
+++ readme.md
@@ -1,22 +1,48 @@
 # とても参考にしたもの
+
 https://www.youtube.com/watch?v=vcSijrRsrY0
 
-intel系CPU対象にしたコンパイラ
-この動画にある、hydrogenという言語がコンパイルできるようにする。
+intel 系 CPU 対象にしたコンパイラ
+この動画にある、hydrogen という言語がコンパイルできるようにする。
 
 # 環境
-VSCodeのdevcontainerを使用する。
-intel系CPUの命令セットの生成、実行を確認するため、
-Dockerfile、devcontainer.jsonでは、`amd64`を指定している。
 
-M1のMacOSでも動くことを目指す(rosetta)。
+VSCode の devcontainer を使用する。
+intel 系 CPU の命令セットの生成、実行を確認するため、
+Dockerfile、devcontainer.json では、`amd64`を指定している。
 
-## devcontainerのはじめかた
-VSCodeで左下のグリーンのアイコンをクリックして、`コンテナで再度開く`をクリックする
+M1 の MacOS でも動くことを目指す(rosetta)。
 
-# https://www.youtube.com/watch?v=vcSijrRsrY0
+## devcontainer のはじめかた
+
+VSCode で左下のグリーンのアイコンをクリックして、`コンテナで再度開く`をクリックする
+
+# https://www.youtube.com/watch?v=pupdmHjzrTI
 
 # 1:00〜命令を変更する
 
 `return 69;`→`exit 69;`
+
+# 3:30〜リファクタリング　 mainのtokenize
+
+- 別ファイルに分割する→tokenization.hpp
+- `i++`、`i--`をやめる
+
+  ```cpp
+  private:
+  [[nodiscard]] std::optional<char> peak(int ahead = 1) const {
+      if (m_index + ahead > m_src.length()) {
+          return {};
+      } else {
+          return m_src.at(m_index);
+      }
+  }
+
+  char consume() { return m_src.at(m_index++); }
+
+  const std::string m_src;
+  int m_index = 0;
+  ```
+
+# 29:00〜リファクタリング　 
 
--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,7 @@
 #include <sstream>
 #include <vector>
 
-enum class TokenType { exit, int_lit, semi };
-
-struct Token {
-  TokenType type;
-  std::optional<std::string> value = {};
-};
+#include "./tokenization.hpp"
 
 std::string tokens_to_asm(const std::vector<Token> &tokens) {
   std::stringstream output;
@@ -32,51 +27,6 @@ std::string tokens_to_asm(const std::vector<Token> &tokens) {
   return output.str();
 }
 
-std::vector<Token> tokenize(const std::string &str) {
-  std::vector<Token> tokens = {};
-  std::string buf = "";
-  for (int i = 0; i < str.length(); i++) {
-    char c = str.at(i);
-    if (std::isalpha(c)) {
-      buf.push_back(c);
-      i++;
-      while (std::isalnum(str.at(i))) {
-        buf.push_back(str.at(i));
-        i++;
-      }
-      i--;
-
-      if (buf == "exit") {
-        tokens.push_back({.type = TokenType::exit});
-        buf.clear();
-        continue;
-      } else {
-        std::cerr << "You messed up!" << std::endl;
-        exit(EXIT_FAILURE);
-      }
-    } else if (std::isdigit(c)) {
-      buf.push_back(c);
-      i++;
-      while (std::isdigit(str.at(i))) {
-        buf.push_back(str.at(i));
-        i++;
-      }
-      i--;
-      tokens.push_back({.type = TokenType::int_lit, .value = buf});
-      buf.clear();
-    } else if (c == ';') {
-      tokens.push_back({.type = TokenType::semi});
-    } else if (std::isspace(c)) {
-      continue;
-    } else {
-      std::cerr << "You messed up!" << std::endl;
-      exit(EXIT_FAILURE);
-    }
-  }
-
-  return tokens;
-}
-
 int main(int argc, char *argv[]) {
   if (argc != 2) {
     std::cerr << "Incorrect usage. Correct usage is..." << std::endl;
@@ -92,7 +42,8 @@ int main(int argc, char *argv[]) {
     contents = contents_stream.str();
   }
 
-  std::vector<Token> tokens = tokenize(contents);
+  Tokenizer tokenizer(std::move(contents));
+  std::vector<Token> tokens = tokenizer.tokenize();
   {
     std::fstream file("out.asm", std::ios::out);
     file << tokens_to_asm(tokens);
